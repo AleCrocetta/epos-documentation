266301e238578e8cdc8a3fae16a0555ad11c19b4
AleCrocetta <alessandro.crocetta@ingv.it>
2025-09-10 16:12:45 +0200

    test10

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -9,19 +9,19 @@ export default [
   },
   {
     path: '/epos-documentation/',
-    component: ComponentCreator('/epos-documentation/', '67f'),
+    component: ComponentCreator('/epos-documentation/', 'b24'),
     routes: [
       {
         path: '/epos-documentation/',
-        component: ComponentCreator('/epos-documentation/', 'd85'),
+        component: ComponentCreator('/epos-documentation/', 'ced'),
         routes: [
           {
             path: '/epos-documentation/',
-            component: ComponentCreator('/epos-documentation/', '11a'),
+            component: ComponentCreator('/epos-documentation/', '8bc'),
             routes: [
               {
-                path: '/epos-documentation/epos-documentation',
-                component: ComponentCreator('/epos-documentation/epos-documentation', '599'),
+                path: '/epos-documentation/index',
+                component: ComponentCreator('/epos-documentation/index', '86e'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               }
